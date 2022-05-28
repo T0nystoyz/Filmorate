0b74936a7545336424f6e1d7d74b4b634e5dbefa
Pavel <pa11ady@yandex.ru>
2022-05-28 19:24:19 +0300

    Model

---
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -0,0 +1,17 @@
+package ru.yandex.practicum.filmorate.model;
+
+import lombok.Data;
+import lombok.NonNull;
+
+import java.time.LocalDate;
+
+@Data
+public class User {
+    private Long id;
+    @NonNull
+    private String email;
+    @NonNull
+    private String login;
+    private String name;
+    private LocalDate birthday;
+}

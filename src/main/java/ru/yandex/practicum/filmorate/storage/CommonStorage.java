6fde559b31f03d59d8cdb61a6232efab35566cc4
Pavel <pa11ady@yandex.ru>
2022-06-12 00:43:50 +0300

    Interface Storage

---
+++ src/main/java/ru/yandex/practicum/filmorate/storage/CommonStorage.java
@@ -0,0 +1,15 @@
+package ru.yandex.practicum.filmorate.storage;
+
+import ru.yandex.practicum.filmorate.model.StorageData;
+
+import java.util.List;
+
+public interface CommonStorage <T extends StorageData> {
+
+    public List<T> findAll();
+
+    public T create(T data);
+
+    public T put(T data);
+
+}

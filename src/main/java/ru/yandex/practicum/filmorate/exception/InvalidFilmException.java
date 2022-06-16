244382452c198fffeea2b5e50f29297416506ed9
Pavel <pa11ady@yandex.ru>
2022-06-16 22:32:43 +0300

    ErrorHandler

--- src/main/java/ru/yandex/practicum/filmorate/exception/InvalidFilmException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/InvalidFilmException.java
@@ -3,8 +3,8 @@ package ru.yandex.practicum.filmorate.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-//Тесты из ТЗ пропускают код ошибки 400
-@ResponseStatus(HttpStatus.BAD_REQUEST)
+//Тесты из ТЗ 8 пропускают код ошибки 400
+//@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class InvalidFilmException extends RuntimeException {
     public InvalidFilmException(String message) {
         super(message);

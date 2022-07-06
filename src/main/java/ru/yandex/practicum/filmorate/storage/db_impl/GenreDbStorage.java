d2765b40bc9789ee6ba9de928493411e593fbaab
Pavel <pa11ady@yandex.ru>
2022-07-06 19:38:07 +0300

    GenreDbStorage findById mapToGenre

--- src/main/java/ru/yandex/practicum/filmorate/storage/db_impl/GenreDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/db_impl/GenreDbStorage.java
@@ -1,14 +1,35 @@
 package ru.yandex.practicum.filmorate.storage.db_impl;
 
+import org.springframework.jdbc.core.JdbcTemplate;
 import ru.yandex.practicum.filmorate.model.Genre;
 import ru.yandex.practicum.filmorate.storage.GenreStorage;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 public class GenreDbStorage implements GenreStorage {
+    private final JdbcTemplate jdbcTemplate;
+
+    public GenreDbStorage(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
     @Override
     public Genre findById(Long id) {
-        return null;
+        String sql = "SELECT * FROM GENRE WHERE GENRE_ID = ?";
+        List<Genre> result = jdbcTemplate.query(sql, this::mapToGenre, id);
+        if (result.isEmpty()) {
+            return null;
+        }
+        return result.get(0);
+    }
+
+    private Genre mapToGenre(ResultSet resultSet, int rowNum) throws SQLException {
+        Genre genre = new Genre();
+        genre.setId(resultSet.getLong("id"));
+        genre.setName(resultSet.getString("name"));
+        return genre;
     }
 
     @Override
